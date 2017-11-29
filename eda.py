47fa7ce8233955e0c4013b8d9cae3091344e7fed
TomaszStepien <tomaszstepien03@gmail.com>
2017-11-29 16:26:48 +0100

    something

--- eda.py
+++ eda.py
@@ -9,12 +9,19 @@ sweaters = pd.read_csv("sweaters.tsv", sep="\t")
 # print(sweaters.head())
 
 # brand_name
-# print all unique values with counts
-counts = sweaters.loc[:, "brand_name"].value_counts()
+# counts = sweaters.loc[:, "brand_name"].value_counts()
 # print(counts.iloc[:300,])
 
-print(counts[counts > 500])
+# print(counts[counts > 500])
 
 # there are 4809 unique brand names (4810 including nans)
 # probably should be trimmed -
 # brands with counts lower than some threshold should go to "other" category
+
+
+# category_name
+# no z tego to mozna zrobic w chuj zmiennych
+# print(sweaters.loc[:, "category_name"])
+
+
+print(sweaters.loc[:,"MODEL_brand_nan.1"].value_counts())
