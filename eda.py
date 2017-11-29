1106636be00138f4a1b6a9666fe1720e8cc3feea
TomaszStepien <tomaszstepien03@gmail.com>
2017-11-29 13:32:04 +0100

    brand_name

--- eda.py
+++ eda.py
@@ -1,1 +1,20 @@
+import pandas as pd
+# import numpy as np
+import os
 
+os.chdir("C:\\kaggle_mercari")
+
+sweaters = pd.read_csv("sweaters.tsv", sep="\t")
+
+# print(sweaters.head())
+
+# brand_name
+# print all unique values with counts
+counts = sweaters.loc[:, "brand_name"].value_counts()
+# print(counts.iloc[:300,])
+
+print(counts[counts > 500])
+
+# there are 4809 unique brand names (4810 including nans)
+# probably should be trimmed -
+# brands with counts lower than some threshold should go to "other" category
